cb234235359adf41e3c801bdc89b1549a22f2c72
Kingicey <iceyokolo@gmail.com>
2022-07-05 09:48:07 -0700

    hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,17 +4,19 @@
  * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
-	if (c > 0 || c == 0)
-	{
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
-	else
-	{
-	c = c * -1;
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }

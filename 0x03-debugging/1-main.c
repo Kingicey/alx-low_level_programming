bc870c9eebdd8457f43f8e51dc3067f02c120571
Kingicey <iceyokolo@gmail.com>
2022-07-06 01:14:43 -0700

    Debugging task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,27 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-        i = 0;
+i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+/*
+* while (i < 10)
+*{
+*        putchar(i);
+*}
+*/
 
-        printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+return (0);
 }
+
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
